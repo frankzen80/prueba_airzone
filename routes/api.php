eda244aca38bfb029fc2f67ce493c4759ba385c0
paco.fernandez <paco.fernandez@bntbusiness.es>
2024-06-06 18:11:26 +0200

    Algunos arrelgos

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 
-Route::get('/user', function (Request $request) {
+/* Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+*/
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-Route::apiResource('api/v1/categories', CategoryController::class);
+Route::apiResource('categories', CategoryController::class)->except([
+    'index', 'store', 'show', 'update', 'destroy'
+]);
